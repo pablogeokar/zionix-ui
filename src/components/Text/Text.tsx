a60ae803be78b0e866b5bc0257518a986078e061
Pablo George <pablogeokar@hotmail.com>
2022-12-31 13:12:46 -0300

    iniciando o Text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,6 +1,16 @@
-import styled, { css } from "styled-components";
+import React from 'react';
+//import styled, { css } from "styled-components";
 import '../../styles/Zionix.css'
 
-export const Text = styled.span`
-  color: var(--primary);
-`
+
+interface TextProps {
+  children: React.ReactNode
+}
+
+export default function Text({ children }: TextProps) {
+  return (
+    <span style={{color: 'var(--primary)'}}>
+      {children}
+    </span>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
 export { default as Button } from "./Button";
-export { Text } from "./Text";
+export { default as Text } from "./Text";
