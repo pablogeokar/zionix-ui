e4c20bcb39fc77c7b60514b3e5ec483722c5a963
Pablo George <pablogeokar@hotmail.com>
2022-12-31 13:04:42 -0300

    criando o Zionix.css

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const dts = require("rollup-plugin-dts");
+const postcss = require("rollup-plugin-postcss");
 
 const packageJson = require("./package.json");
 
@@ -24,11 +25,13 @@ module.exports = [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts.default()],
+    external: [/\.css$/],
   },
 ];
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,6 @@
+import styled, { css } from "styled-components";
+import '../../styles/Zionix.css'
+
+export const Text = styled.span`
+  color: var(--primary);
+`
