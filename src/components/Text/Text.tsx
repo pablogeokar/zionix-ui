5b7a5a87de9ee2e5e04458199b6c22c29db9c1bf
Pablo George <pablogeokar@hotmail.com>
2023-01-01 18:47:09 -0300

    tratando o componente Text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,31 +2,32 @@
 import React from 'react';
 
 const TextSizes = {
-  xsmall: "var(--text-xsmall, '1rem')",
-  small: "var(--text-small, '1.2rem')",
-  normal: "var(--text-normal, '1.6rem')",
-  large: "var(--text-large, '3.2rem')",
-  xlarge: "var(--text-xlarge, '3.2rem')",
-  xxlarge: "var(--text-xxlarge, '4rem')",
+  xsmall: "var(--text-xsmall, 1rem)",
+  small: "var(--text-small, 1.2rem)",
+  normal: "var(--text-normal, 1.6rem)",
+  large: "var(--text-large, 2.1rem)",
+  xlarge: "var(--text-xlarge, 3.2rem)",
+  xxlarge: "var(--text-xxlarge, 4rem)",
 }
 
 export interface TextProps {
   size?: keyof typeof TextSizes;
   children?: React.ReactNode;
-  level?: 1 | 2 | 3 | 4 | 5 | 6
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+  lineHeight?: string | number;
 }
 
-export default function Text({ size, children, level }: TextProps) {
-  const fontSize = TextSizes[size || 'normal']
+export default function Text({ size, children, level, fontWeight, lineHeight }: TextProps) {
   return (
     <>
-      {!level && <span style={{ fontSize: fontSize }}>{children}</span>}
-      {level === 1 && <h1 style={{ fontSize: size || TextSizes['xxlarge'] }}>{children}</h1>}
-      {level === 2 && <h2 style={{ fontSize: size || TextSizes['xlarge'] }}>{children}</h2>}
-      {level === 3 && <h3 style={{ fontSize: size || TextSizes['large'] }}>{children}</h3>}
-      {level === 4 && <h4 style={{ fontSize: size || TextSizes['normal'] }}>{children}</h4>}
-      {level === 5 && <h5 style={{ fontSize: size || TextSizes['small'] }}>{children}</h5>}
-      {level === 6 && <h6 style={{ fontSize: size || TextSizes['xsmall'] }}>{children}</h6>}
+      {!level && <span style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</span>}
+      {level === 1 && <h1 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xxlarge'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1 }}>{children}</h1>}
+      {level === 2 && <h2 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xlarge'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5 }}>{children}</h2>}
+      {level === 3 && <h3 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['large'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</h3>}
+      {level === 4 && <h4 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</h4>}
+      {level === 5 && <h5 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['small'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5 }}>{children}</h5>}
+      {level === 6 && <h6 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xsmall'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1.5 }}>{children}</h6>}
     </>
   )
 }
--- src/context/context.ts
+++ src/context/context.ts
@@ -10,7 +10,7 @@ export const INITIAL_STATE = {
     xsmall: "1rem",
     small: "1.2rem",
     normal: "1.6rem",
-    large: "2.4rem",
+    large: "2.1rem",
     xlarge: "3.2rem",
     xxlarge: "4rem",
   },
