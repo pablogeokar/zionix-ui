04ec37d3de44418dea6fb0cd70f12f5c527072e0
Pablo George <pablogeokar@hotmail.com>
2023-01-01 23:24:52 -0300

    consertando o Text component

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Import into `pages/_app.ts` ThemeContextProvider and ThemeFont
 First you will import the ThemeContextProvider and ThemeFont to the main page of your react project as in the example below,
 ThemeFont is optional, if you already have your own font loading routine, this component can be safely left out.
 
-Primeiro você irá importar o ThemeContextProvider e o ThemeFont para a página principal do seu projeto react como no exemplo abaixo, 
+Primeiro você irá importar o ThemeContextProvider e o ThemeFont para a página principal do seu projeto react como no exemplo abaixo,
 o ThemeFont é opcional, caso você já tenha sua própria rotina de carregamento de fontes, este componente poderá ficar de fora tranquilamente.
 
 ```js
@@ -23,4 +23,16 @@ import { ThemeContextProvider, ThemeFont } from "@zionix/ui";
     <Component {...pageProps} />
   </ThemeContextProvider>
 </>;
+```
+
+## Component `Text`
+
+```js
+  <Text level={1} fontWeight={700}>Text Style h1</Text>
+  <Text level={2} lineHeight={1.6}>Text Style h2</Text>
+  <Text level={3} size="xlarge">Text Style h3</Text>
+  <Text>Text span</Text>
+  <Text level={4}>Text Style h4</Text>
+  <Text level={5}>Text Style h5</Text>
+  <Text level={6}>Text Style h1</Text>
 ```
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -10,24 +10,28 @@ const TextSizes = {
   xxlarge: "var(--text-xxlarge, 4rem)",
 }
 
+
 export interface TextProps {
   size?: keyof typeof TextSizes;
   children?: React.ReactNode;
   level?: 1 | 2 | 3 | 4 | 5 | 6;
-  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | "bold";
   lineHeight?: string | number;
+  onClick?: () => void
+  style?: {}
+  className?: string
 }
 
-export default function Text({ size, children, level, fontWeight, lineHeight }: TextProps) {
+export default function Text({ size, children, level, fontWeight, lineHeight, style, className, onClick }: TextProps) {
   return (
     <>
-      {!level && <span style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</span>}
-      {level === 1 && <h1 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xxlarge'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1 }}>{children}</h1>}
-      {level === 2 && <h2 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xlarge'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5 }}>{children}</h2>}
-      {level === 3 && <h3 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['large'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</h3>}
-      {level === 4 && <h4 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5 }}>{children}</h4>}
-      {level === 5 && <h5 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['small'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5 }}>{children}</h5>}
-      {level === 6 && <h6 style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: size || TextSizes['xsmall'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1.5 }}>{children}</h6>}
+      {!level && <span className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5, ...style }}>{children}</span>}
+      {level === 1 && <h1 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'xxlarge'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1, ...style }}>{children}</h1>}
+      {level === 2 && <h2 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'xlarge'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5, ...style }}>{children}</h2>}
+      {level === 3 && <h3 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'large'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5, ...style }}>{children}</h3>}
+      {level === 4 && <h4 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'normal'], fontWeight: fontWeight || 400, lineHeight: lineHeight || 1.5, ...style }}>{children}</h4>}
+      {level === 5 && <h5 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'small'], fontWeight: fontWeight || 500, lineHeight: lineHeight || 1.5, ...style }}>{children}</h5>}
+      {level === 6 && <h6 className={className} onClick={onClick} style={{ display: 'flex', alignItems: 'center', gap: '1rem', fontSize: TextSizes[size || 'xsmall'], fontWeight: fontWeight || 600, lineHeight: lineHeight || 1.5, ...style }}>{children}</h6>}
     </>
   )
 }
