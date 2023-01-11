55144937e131e3f54c625429ae221631db0dbbbf
Pablo George <pablogeokar@hotmail.com>
2023-01-11 15:36:56 -0300

    ajustes no componente Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -4,25 +4,25 @@ import { IconsType } from './IconsType';
 interface IconProps extends IconsType {
   size?: number;
   weight?: 'bold' | 'duotone' | 'fill' | 'light' | 'regular' | 'thin';
+  className?: string;
 }
 
-export default function Icon({ iconName, weight = 'thin', size = 28 }: IconProps) {
+export default function Icon({ iconName, className, weight = 'thin', size = 28 }: IconProps) {
   try {
     const PhosphorIcon = require(`../../../../phosphor-react`)[iconName]
 
     return (
-      <>
+      <div className={className}>
         {PhosphorIcon && <PhosphorIcon size={size} weight={weight} />}
-      </>
+      </div>
     )
   } catch {
     const DevPhosphorIcon = require(`../../node_modules/phosphor-react`)[iconName] || null
     return (
-      <>
+      <div className={className}>
         {/**❌ modo link em ambiente de desenvolvedor */}
         {DevPhosphorIcon && <DevPhosphorIcon size={size} weight={weight} />}
-      </>
-
+      </div>
     )
   }
 
